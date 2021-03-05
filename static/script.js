222e701469ae01ef721d986ba16fa5782b2f32e3
GerMorales <20195358@esen.edu.sv>
2021-03-05 11:53:45 -0600

    added new function

--- static/script.js
+++ static/script.js
@@ -123,6 +123,20 @@ function logout() {
     //window.location.href = "http://heroku:5000/";
 }
 
+function hideDivById(divId) {
+    hideAllDivW3Includes()
+    var element = document.getElementById(divId)
+    if (element.style.display === "none") {
+        element.style.display = "block"
+    }
+}
+
+function hideAllDivW3Includes() {
+    var elementArray = document.getElementsByName("w3includes")
+    for (var element of elementArray) {
+        element.style.display = "none"
+    }
+}
 /*
 ************* dashboard functionality end
 */
